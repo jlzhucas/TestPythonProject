0510a62f28b091bd0252a3e404769f66075c564d
Jinliang Zhu <zhujinlianghust@gmail.com>
2013-03-23 22:36:01 +0800

    Update helloworld.py

--- TestPythonProject/src/helloworld.py
+++ TestPythonProject/src/helloworld.py
@@ -8,6 +8,8 @@ Created on Mar 23, 2013
 class Test(object):
     def get_test_name(self):
         return "hello world!"
-    
-t = Test()
-print t.get_test_name()
+
+
+if __name__ == "__main__":
+    t = Test()
+    print t.get_test_name()
